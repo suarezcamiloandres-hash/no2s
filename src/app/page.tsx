24c8ec49dc27f59ee03154070674457aec160b6d
suarezcamiloandres <suarez.camiloandres@gmail.com>
2025-10-15 22:36:42 -0500

    04:Authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,22 @@
-import { getQueryClient, trpc } from '@/trpc/server';
-import { Client } from "./client";
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-import { Suspense } from 'react';
+import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
+import { LogoutButton } from "./logout";
 
 
-const Page = async () => {
-  const queryClient = getQueryClient(); 
+const Page = async ()=> {
+  await requireAuth();
+
+  const data = await caller.getUsers();
  
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
-
-  const dehydratedState = dehydrate(queryClient);
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Client />
-        </Suspense>
-      </HydrationBoundary>
+    <div className="min-h-screen flex items-center justify-center flex-col gap-y-6">
+      Protected server component
+      <div>
+        {JSON.stringify(data, null, 2)}
+      </div>
+      <LogoutButton />
     </div>
-    );
+  );
 };
 
 export default Page;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,14 @@
-import { baseProcedure, createTRPCRouter } from '../init';
+import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
+
 export const appRouter = createTRPCRouter({
-  getUsers: baseProcedure.query(() => {
-      return prisma.user.findMany;
-    }),
+  getUsers: protectedProcedure.query(async ({ ctx }) => {
+    return await prisma.user.findMany({
+      where: {  
+        id: ctx.session.user.id,
+      },
+    });
+  }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
